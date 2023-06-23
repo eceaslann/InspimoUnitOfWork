a9fe194325f2fa8311454b94e140d52aed83311c
Ece Aslan <eceaslan296@gmail.com>
2023-06-23 12:01:09 +0300

    Proje dosyası ekle.

---
+++ InspimoUnitOfWork/Controllers/SendMoneyController.cs
@@ -0,0 +1,41 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using UnitOfWorkDesignPattern.Models;
+
+namespace UnitOfWorkDesignPattern.Controllers
+{
+    public class SendMoneyController : Controller
+    {
+        private readonly ICustomerService _customerService;
+
+        public SendMoneyController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(CustomerViewModel model)
+        {
+            var value1 = _customerService.TGetByID(model.SenderID);
+            var value2 = _customerService.TGetByID(model.ReceiverID);
+
+            value1.CustomerBalanca -= model.Amount;
+            value2.CustomerBalanca += model.Amount;
+
+            List<Customer> modifiedCustomers = new List<Customer>()
+            {
+                value1, value2
+            }; 
+
+            _customerService.TMultiUpdate(modifiedCustomers);
+            return View();
+
+        }
+    }
+}
